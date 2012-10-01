48a231a0a94ea28cd38ac0f96db29710d7c92161
Richard Morris <richard.morris@gettyimages.com>
2012-09-30 23:32:52 -0700

    Remove redundant code

--- lib/puree.rb
+++ lib/puree.rb
@@ -12,30 +12,4 @@ require 'puree/event_bus/subscriber'
 require 'puree/event_bus/memory_event_bus'
 
 module Puree
-
-  def self.config
-    Configuration.instance
-  end
-
-  class Configuration
-  	include Singleton
-
-  	attr_accessor :id_generator, :event_store, :event_bus
-
-  	def id_generator
-      if @id_generator.nil?
-        raise 'Puree.config.id_generator has not been configured'
-      end
-      @id_generator
-    end
-
-    def event_store
-      if @event_store.nil?
-        raise 'Puree.config.event_store has not been configured'
-      end
-      @event_store
-    end
-
-  end
-
 end
